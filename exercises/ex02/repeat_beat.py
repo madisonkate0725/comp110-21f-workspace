3c7b467a27d11d66aff31bb1c27bf61800f49399
Madison Williams <madikate@live.unc.edu>
2021-09-07 14:46:49 -0400

    ex 02

--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -8,9 +8,14 @@ __author__ = "730331607"
 word_repeated: str = str(input("What beat do you want to repeat? "))
 number_of_times_repeated: int = int(input("How many times do you want to repeat it? "))
 counter: int = 0
-
-while counter < number_of_times_repeated:
-    counter = counter + 1
-print((word_repeated + " ") * counter)
-if counter <= 0:
+if number_of_times_repeated <= 0:
     print("No beat...")
+    
+else:
+    product: str = word_repeated
+    while counter < number_of_times_repeated - 1:
+        product = word_repeated + " " + product
+        counter = counter + 1
+    print(product)
+
+
